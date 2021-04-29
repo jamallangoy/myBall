2e293e7a3b162395dcae24e4c59f5e01f2d1a28a
jamallangoy <81915438+jamallangoy@users.noreply.github.com>
2021-04-29 14:57:57 -0400

    Delete myBall.js

--- myBall.js
+++
@@ -1,9 +0,0 @@
-<div id="ball" style="
-    z-index:5;
-    position: absolute;
-    top:0px;
-    left:0px;
-    height: 100px;
-    width: 100px;
-    border-radius: 50%
-    background: yellow"></div>
